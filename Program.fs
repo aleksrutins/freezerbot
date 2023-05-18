aff231956f8fb2d90165768133a48731259aa204
Aleks Rutins <aleks@rutins.com>
2023-05-18 07:11:09 -0400

    It doesn't work

--- Program.fs
+++ Program.fs
@@ -10,20 +10,67 @@ let log msg =
     printfn "%s" (msg.ToString())
     Task.CompletedTask
 
-let mutable client = null
+let mutable client: DiscordSocketClient = null
+let guildId = ""
+
+let slashOpt name t desc required = (new SlashCommandOptionBuilder()).WithName(name).WithType(t).WithDescription(desc).WithRequired(required)
+
+let registerCommands () =
+    Async.Parallel ([|
+        (new SlashCommandBuilder())
+            .WithName("freeze")
+            .WithDescription("Freeze the codebase.")
+            .Build()
+
+        
+        (new SlashCommandBuilder())
+            .WithName("thaw")
+            .WithDescription("Unfreeze the codebase.")
+            .Build()
+
+        (new SlashCommandBuilder())
+            .WithName("request-unfreeze")
+            .WithDescription("Request a freeze exception for a branch.")
+            .AddOption(slashOpt "branch" ApplicationCommandOptionType.String "The branch to create a request for" true)
+            .Build()
+        
+        (new SlashCommandBuilder())
+            .WithName("status")
+            .WithDescription("View the current status of the codebase.")
+            .Build()
+        
+        (new SlashCommandBuilder())
+            .WithName("review")
+            .WithDescription("Review an unfreeze request.")
+            .AddOption(slashOpt "branch" ApplicationCommandOptionType.String "The branch to review for" true)
+            .AddOption(
+                (new SlashCommandOptionBuilder())
+                    .WithName("review")
+                    .WithDescription("Your review")
+                    .WithRequired(true)
+                    .AddChoice("reject", 0)
+                    .AddChoice("accept", 0)
+                    .WithType(ApplicationCommandOptionType.Integer)
+            )
+            .Build()
+    |] |> Array.map client.CreateGlobalApplicationCommandAsync |> Array.map Async.AwaitTask)
+
+let clientReady () =
+    registerCommands () |> Async.Ignore |> Async.StartAsTask
 
 let mainAsync () =
-    async {
-        client <- new DiscordSocketClient()
-        client.add_Log log
-        let token = Environment.GetEnvironmentVariable("DISCORD_TOKEN")
-        client.LoginAsync(TokenType.Bot, token) |> Async.AwaitTask |> ignore
-        client.StartAsync() |> Async.AwaitTask |> ignore
-        Task.Delay -1 |> Async.AwaitTask |> ignore
-    }
+    client <- new DiscordSocketClient()
+    client.add_Log log
+    client.add_Ready clientReady
+    let token = Environment.GetEnvironmentVariable("DISCORD_TOKEN")
+    Async.Sequential ([|
+        client.LoginAsync(TokenType.Bot, token)
+        client.StartAsync()
+        Task.Delay -1
+    |] |> Array.map(Async.AwaitTask))
 
 [<EntryPoint>]
 let main argv =
     DotEnv.Load()
-    mainAsync () |> Async.RunSynchronously
+    mainAsync () |> Async.RunSynchronously |> ignore
     0
